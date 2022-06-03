69f61fd2f6f2477c60bb559458468bf8ea0e2612
Shlok <agarwalshlok89@gmail.com>
2022-06-04 01:23:14 +0530

    second commit

---
+++ first.py
@@ -0,0 +1,14 @@
+# high level language , it manages memory on its own , portable language
+# chapter 1 : modeules , comments and pip
+import os  # built in module
+# external module playsound installed using pip on our system and then imported to directly use in our program
+from playsound import playsound
+print("hello world")
+'''multi line comment 
+   Triple single quote is used to write a multi line comment
+'''
+print('''twinkle twinkle little star how i wonder what you are
+up above the world so high like a diamond in the sky''')
+# To print a multiline string you need to use triple single quotes
+# playsound('C:\\Users\Shlok\Downloads\play.mp3')
+print(os.listdir())
